82247e5c58e414af92d880908882a850a753722b
gnat <gnattali@gmail.com>
2014-04-01 01:21:14 +0900

    assets.paths for detail-admin and redactor

--- lib/gnoibox/engine.rb
+++ lib/gnoibox/engine.rb
@@ -1,7 +1,17 @@
 module Gnoibox
   class Engine < ::Rails::Engine
     isolate_namespace Gnoibox
-    
+
+    initializer "assets.path" do |app|
+      app.config.assets.paths << root.join("vendors", "detail-admin", "js")
+      app.config.assets.paths << root.join("vendors", "detail-admin", "css")
+      app.config.assets.paths << root.join("vendors", "detail-admin", "img")
+      app.config.assets.paths << root.join("vendors", "detail-admin", "font")
+
+      app.config.assets.paths << root.join("vendors", "redactor", "javascripts")
+      app.config.assets.paths << root.join("vendors", "redactor", "stylesheets")
+    end
+
     config.to_prepare do
       ::ApplicationController.helper(Gnoibox::ApplicationHelper)
     end
