fc54275f6272345c8e90b6b773c48e33779e7c1b
gnat <gnattali@gmail.com>
2014-04-04 17:12:34 +0900

    order items by order_value

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140325033951) do
+ActiveRecord::Schema.define(version: 20140404075641) do
 
   create_table "gnoibox_authors", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20140325033951) do
     t.string   "main_image"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "order_value"
   end
 
   create_table "taggings", force: true do |t|
