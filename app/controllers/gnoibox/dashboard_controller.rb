531e2ce72fdee549ad0b4c4048b82a5912c781d9
gnat <gnattali@gmail.com>
2014-04-11 18:56:09 +0900

    admin view

--- app/controllers/gnoibox/dashboard_controller.rb
+++ app/controllers/gnoibox/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Gnoibox::DashboardController < Gnoibox::ApplicationController
 
   def index
+    @boxes = Gnoibox::BoxCollection.all.sort_by{|b| b.key==:root ? 0 : 1 }
+    @blocks = Gnoibox::BlockCollection.all
   end
 
 end
