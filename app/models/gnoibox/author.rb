680e8807ea02702ef017cf7d7276bbb11e99d4fa
gnat <gnattali@gmail.com>
2014-04-15 19:48:54 +0900

    author model

--- app/models/gnoibox/author.rb
+++ app/models/gnoibox/author.rb
@@ -1,5 +1,14 @@
 module Gnoibox
   class Author < ActiveRecord::Base
+    include ColumnHoldable
+
+    self.table_name= :gnoibox_authors
+
+    validates :name, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+
+    mount_uploader :image, MainImageUploader
+
     # Include default devise modules. Others available are:
     # :confirmable, :registerable, :lockable, :timeoutable and :omniauthable
     devise :database_authenticatable,
