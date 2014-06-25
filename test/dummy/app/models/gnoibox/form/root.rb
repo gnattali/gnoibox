ffd1481db94eb5e22397ab0cd8d87b94beb7ffee
gnat <gnattali@gmail.com>
2014-06-25 15:41:44 +0900

    select column include_blank option

--- test/dummy/app/models/gnoibox/form/root.rb
+++ test/dummy/app/models/gnoibox/form/root.rb
@@ -7,10 +7,11 @@ module Gnoibox
     set_col :name, :text, 'お名前', validates: {presence: true}
     set_col :email, :text, 'メールアドレス', validates: { presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ } }
 
-    set_col :select_col, :select, 'selectサンプル', options: {a: 'A', b: 'B', c: 'C'}
+    set_col :select_col, :select, 'selectサンプル', options: {a: 'A', b: 'B', c: 'C'}, include_blank: true
     set_col :radio_col, :radio, 'radioサンプル', options: {a: 'A', b: 'B', c: 'C'}
     set_col :check_box_col, :check_box, 'check_boxサンプル', options: {a: 'A', b: 'B', c: 'C'}
-
+    set_col :boolean_col, :boolean, 'booleanサンプル'
+    
     set_col :description, :text_area, 'お問い合わせ内容'
   end
 end
