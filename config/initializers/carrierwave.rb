37e28124e2c26a8ffa877d4a418faed4b74a2fce
gnat <gnattali@gmail.com>
2014-07-22 01:22:05 +0900

    configure S3 only when available

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,24 +1,27 @@
-# define in app/models/gnoibox/s3.rb
-# module Gnoibox
-#   module S3
-#     ACCESS_KEY_ID = ''
-#     SECRET_ACCESS_KEY = ''
-#     REGION = ''
-#     BUCKET = ''
+# configure in config/application.rb
+#
+# config.before_initialize do
+#   Gnoibox::S3.configure do |config|
+#     config.access_key_id = ENV['S3_ACCESS_KEY_ID']
+#     config.secret_access_key = ENV['S3_SECRET_ACCESS_KEY']
+#     config.region = ENV['S3_REGION']
+#     config.bucket = ENV['S3_BUCKET']
 #   end
 # end
 
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :aws_access_key_id => Gnoibox::S3.access_key_id,
-    :aws_secret_access_key => Gnoibox::S3.secret_access_key,
-    :provider => 'AWS',
-    :region => Gnoibox::S3.region
-  }
-  config.fog_directory = Gnoibox::S3.bucket
-  config.fog_public = true
-end
 
+if Gnoibox::S3.access_key_id
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      :aws_access_key_id => Gnoibox::S3.access_key_id,
+      :aws_secret_access_key => Gnoibox::S3.secret_access_key,
+      :provider => 'AWS',
+      :region => Gnoibox::S3.region
+    }
+    config.fog_directory = Gnoibox::S3.bucket
+    config.fog_public = true
+  end
+end
 
 module CarrierWave
   module MiniMagick
