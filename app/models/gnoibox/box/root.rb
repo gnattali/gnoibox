e66110e148b8fc78f0e8c37c52e820f897608a4c
gnat <gnattali@gmail.com>
2014-09-11 21:24:26 +0900

    detect root box custom template

--- app/models/gnoibox/box/root.rb
+++ app/models/gnoibox/box/root.rb
@@ -1,4 +1,6 @@
-#can be overridden
+#can be overridden in app/models/gnoibox/box/root.rb
+#or
+#decorated using class_eval in app/decorators/models/gnoibox/box/root_decorator.rb
 module Gnoibox
   class Box::Root < Box
     set_key :root, '固定ページ'
@@ -7,7 +9,8 @@ module Gnoibox
     set_position 0
 
     def self.member_view(url_parser)
-      url_parser.params[:first]
+      url = url_parser.params[:first]
+      Dir.glob("#{Rails.application.config.root}/app/views/gnoibox/site/#{url}.*").present? ? url : @member_view
     end
   end
 
