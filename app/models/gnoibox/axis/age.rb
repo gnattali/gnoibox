d076fae2ae6e8d0212ea1dae639f3e4da69de499
gnat <gnattali@gmail.com>
2015-01-11 00:06:36 +0900

    future date range for age axis

--- app/models/gnoibox/axis/age.rb
+++ app/models/gnoibox/axis/age.rb
@@ -4,8 +4,8 @@ module Gnoibox
     
     set_key :age, '経過年数'
 
-    set_option :in_3year, '3年以内', range: (0..3)
-    set_option :in_5year, '5年以内', range: (0..5)
-    set_option :in_10year, '10年以内', range: (0..10)
+    set_option :in_3year, '3年以内', range: (-10000..3)
+    set_option :in_5year, '5年以内', range: (-10000..5)
+    set_option :in_10year, '10年以内', range: (-10000..10)
   end
 end
