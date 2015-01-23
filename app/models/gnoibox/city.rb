123fb345906e7a5ae164ab0b9188606246464bc3
gnat <gnattali@gmail.com>
2015-01-23 15:30:28 +0900

    switch from instance variable memoize to rails cache

--- app/models/gnoibox/city.rb
+++ app/models/gnoibox/city.rb
@@ -13,31 +13,41 @@ module Gnoibox
     end
     
     def self.arrays
-      @arrays ||= File.open(File.join(Gnoibox::Engine.root, "db", "seeds", "cities.dump"), 'r'){|f| Marshal.load(f) }
+      Rails.cache.fetch("gnoibox/city/arrays"){ File.open(File.join(Gnoibox::Engine.root, "db", "seeds", "cities.dump"), 'r'){|f| Marshal.load(f) } }
+      # @arrays ||= File.open(File.join(Gnoibox::Engine.root, "db", "seeds", "cities.dump"), 'r'){|f| Marshal.load(f) }
     end
     
     def self.axis_options
-      @axis_options ||= begin
+      Rails.cache.fetch("gnoibox/city/axis_options") do
         arrays.map do |r|
           Gnoibox::Axis::Option.new r[3], r[2], {prefecture_id: r[0], city_id: r[1]}
         end
       end
+      # @axis_options ||= begin
+      #   arrays.map do |r|
+      #     Gnoibox::Axis::Option.new r[3], r[2], {prefecture_id: r[0], city_id: r[1]}
+      #   end
+      # end
     end
 
     def self.option_keys
-      @option_keys ||= axis_options.map(&:key)
+      Rails.cache.fetch("gnoibox/city/option_keys"){ axis_options.map(&:key) }
+      # @option_keys ||= axis_options.map(&:key)
     end
     
     def self.option_hash
-      @option_hash ||= axis_options.index_by(&:key)
+      Rails.cache.fetch("gnoibox/city/options_hash"){ axis_options.index_by(&:key) }
+      # @option_hash ||= axis_options.index_by(&:key)
     end
     
     def self.text_hash
-      @text_hash ||= axis_options.index_by(&:label)
+      Rails.cache.fetch("gnoibox/city/text_hash"){ axis_options.index_by(&:label) }
+      # @text_hash ||= axis_options.index_by(&:label)
     end
     
     def self.text_list
-      @text_list ||= text_hash.keys
+      Rails.cache.fetch("gnoibox/city/text_list"){ text_hash.keys }
+      # @text_list ||= text_hash.keys
     end
     
   end
--- app/models/gnoibox/station.rb
+++ app/models/gnoibox/station.rb
@@ -14,7 +14,8 @@ module Gnoibox
     
     def self.arrays
       #id,title,prefecture_id,station_key
-      @arrays ||= File.open(File.join(Gnoibox::Engine.root, "db", "seeds", "stations.dump"), 'r'){|f| Marshal.load(f) }
+      Rails.cache.fetch("gnoibox/station/arrays")fetch{ File.open(File.join(Gnoibox::Engine.root, "db", "seeds", "stations.dump"), 'r'){|f| Marshal.load(f) } }
+      # @arrays ||= File.open(File.join(Gnoibox::Engine.root, "db", "seeds", "stations.dump"), 'r'){|f| Marshal.load(f) }
     end
 
     def self.gnoibox_key_from(key)
@@ -27,20 +28,24 @@ module Gnoibox
     end
 
     def self.axis_options
-      @axis_options ||= arrays.map{|r| Gnoibox::Station::Option.new gnoibox_key_from(r[3]), r[1], r[0] }
+      Rails.cache.fetch("gnoibox/station/axis_options"){ arrays.map{|r| Gnoibox::Station::Option.new gnoibox_key_from(r[3]), r[1], r[0] } }
+      # @axis_options ||= arrays.map{|r| Gnoibox::Station::Option.new gnoibox_key_from(r[3]), r[1], r[0] }
     end
 
     def self.option_keys
-      @option_keys ||= axis_options.map(&:key)
+      Rails.cache.fetch("gnoibox/station/options_keys"){ axis_options.map(&:key) }
+      # @option_keys ||= axis_options.map(&:key)
     end
 
     def self.option_hash
       #reverse to reflect priority
-      @option_hash ||= axis_options.reverse.index_by(&:key)
+      Rails.cache.fetch("gnoibox/station/option_hash"){ axis_options.reverse.index_by(&:key) }
+      # @option_hash ||= axis_options.reverse.index_by(&:key)
     end
     
     def self.id_hash
-      @id_hash ||= axis_options.index_by{|o| o.station_id.to_sym}
+      Rails.cache.fetch("gnoibox/station/id_hash"){ axis_options.index_by{|o| o.station_id.to_sym} }
+      # @id_hash ||= axis_options.index_by{|o| o.station_id.to_sym}
     end
     
     def self.title_for(id)
@@ -57,7 +62,7 @@ module Gnoibox
     end
     
     def self.suggest_options
-      @suggest_options ||= begin
+      Rails.cache.fetch("gnoibox/station/suggest_options") do
         dic = Gnoibox::Railway.long_title_dictionary
         arrays.map do |s|
           if railway = dic[railway_id_from(s[0])]
@@ -65,6 +70,14 @@ module Gnoibox
           end
         end.compact
       end
+      # @suggest_options ||= begin
+      #   dic = Gnoibox::Railway.long_title_dictionary
+      #   arrays.map do |s|
+      #     if railway = dic[railway_id_from(s[0])]
+      #       [ s[0], railway+" "+s[1]+"駅" ]
+      #     end
+      #   end.compact
+      # end
     end
 
     def self.select2_options_for(q)
@@ -72,9 +85,12 @@ module Gnoibox
     end
     
     def self.railway_hash
-      @railway_hash ||= begin
+      Rails.cache.fetch("gnoibox/station/railway_hash") do
         arrays.map{|s| [railway_id_from(s[0]), gnoibox_key_from(s[3])] }.group_by{|s| s[0] }.to_a.map{|r| [Gnoibox::Railway.key_for(r[0]), r[1].map{|s| s[1]} ] }.to_h
       end
+      # @railway_hash ||= begin
+      #   arrays.map{|s| [railway_id_from(s[0]), gnoibox_key_from(s[3])] }.group_by{|s| s[0] }.to_a.map{|r| [Gnoibox::Railway.key_for(r[0]), r[1].map{|s| s[1]} ] }.to_h
+      # end
     end
 
     class Option
